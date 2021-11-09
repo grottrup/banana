98e0e7c5e2ae6f86252695ab7c4269c7cadb277f
ravvnen <phillip.boe.jensen@gmail.com>
2021-11-09 14:39:46 +0100

    " view + url update"

--- src/django_project/UserHub/views.py
+++ src/django_project/UserHub/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.models import User
 from django.contrib import messages
 #from UserHub.forms import UserSignUpForm, EmailMessageForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+class DashboardView(TemplateView):
+    template_name= 'dashboard.html'
