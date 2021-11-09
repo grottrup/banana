42877e8344f91318d3f006c46921ea08e5563219
ravvnen <phillip.boe.jensen@gmail.com>
2021-11-09 14:39:10 +0100

    "login/logout url + views"

--- src/django_project/UserHub/urls.py
+++ src/django_project/UserHub/urls.py
@@ -1,2 +1,25 @@
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views 
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+
+    #Path for Login
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html'
+    ),
+    name='login'),
+
+    #Path for Logout
+    path('logout/', auth_views.LogoutView.as_view(
+        
+        # Site you are sent to when logging out - @Karsten, you need to match this keyword
+        next_page='dashboard'
+    ),
+    name='logout')
+
+]
 
