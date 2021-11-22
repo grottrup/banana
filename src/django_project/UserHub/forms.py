3e107227b4b43817120f9c3761553804355036c3
Artin13 <artin2012@live.dk>
2021-11-22 16:09:38 +0100

    send mail funciton + forms.py update

--- src/django_project/UserHub/forms.py
+++ src/django_project/UserHub/forms.py
@@ -43,3 +43,65 @@ class AccountAuthenticationForm(forms.ModelForm):
 			password = self.cleaned_data['password']
 			if not authenticate(email=email, password=password):
 				raise forms.ValidationError("Invalid login")
+
+
+
+class UserSignUpForm(UserCreationForm):
+    # doing this makes the email field a mandatory field for the user to input data
+    email = forms.EmailField()
+
+    class Meta:
+        # this form is to interact with the model/database table below
+        # i.e if the form is saved, save the data to the User table
+        model = User
+        # the form should display the following fields
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+# we will a create a form with a drop down list of users who have signed up
+# the drop down will display the names of the users, but will hold the user emails
+# as the drop down/select value. This is the value that will be submited when a user submits the form
+
+# the form will also have a large text area that can accomodate 2,000 words for the user to type the message
+class EmailMessageForm(forms.Form):
+    # get all active registered users
+    # the values_list data is the data to be held in the drop down i.e. inside the HTML select tags
+    #
+    # <select>
+    #   <option value="id"> User Fullname </option>
+    #   <option value="1"> John Doe </option>
+    # </select>
+    #
+
+    CHOICES = User.objects.all().values_list(   
+                    # the data to be held in the value field of the dropdowns e.g. the HTML option tag
+                    'id',
+    
+                    # the user friendly value that will be displayed to the user is the User Fullname
+                    # which will be a concatination of the users' first_name and last_name
+                    # I am placing the V(' ') between the names simply to create a space during concatination
+                    Concat(
+                            F('first_name'), V(' '), F('last_name'),
+
+                            #how the data is to be outputted
+                            output_field=CharField()
+                        )
+                )
+
+    # the drop down to hold registered active users' (users not deleted/deactivated) fullnames and user database ids
+    # the drop down is to be populated with data from the CHOICES variable above
+    user_id = forms.ChoiceField(choices=CHOICES)
+
+    # the email subject
+    subject = forms.CharField(max_length=100)
+
+    # the email message textarea
+    message = forms.CharField(max_length=2000,widget=forms.Textarea)
+
+
+    #attach = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super(EmailMessageForm, self).__init__(*args, **kwargs) # Call to ModelForm constructor
+        self.fields['subject'].widget.attrs['style'] = 'width:100%;'
+        self.fields['message'].widget.attrs['cols'] = 150
+
--- src/django_project/UserHub/views.py
+++ src/django_project/UserHub/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.models import User
 from django.contrib import messages
-#from UserHub.forms import UserSignUpForm, EmailMessageForm
+from UserHub.forms import UserSignUpForm, EmailMessageForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
@@ -94,3 +94,66 @@ def get_redirect_if_exists(request):
 		if request.GET.get("next"):
 			redirect = str(request.GET.get("next"))
 	return redirect
+
+
+def send_email(request):
+    # do the following when a user, submits the form which sends/returns a POST request to the same view
+    # and sends/returns with it the POST data submited in the form
+    if request.method == 'POST':
+        # instantiate a new form with the POST data submitted
+        form = EmailMessageForm(request.POST)
+        # check if the submitted form data is valid
+        if form.is_valid():
+            # get the submitted subject
+            subject = form.cleaned_data['subject']
+
+            # get the submitted message
+            message = form.cleaned_data['message']
+
+            # get the submitted user_id, which is the value of the option that was selected and submitted
+            user_id = form.cleaned_data['user_id']
+
+            # get the user associated with the passed user_id
+            user = User.objects.get(id=user_id)
+
+            #file = request.FILES['file']
+            #attach = user.attach(file.name, file.read(), file.content_type())
+            #email = EmailMessage(subject,message,EMAIL_HOST_USER, user_id) 
+            #email.content_subtype = 'html'
+
+            
+            #email.send()
+            
+            # call the built in email_user method on the returned user object
+            # enter the email subject, message and the name of whom the email is from
+            # set it to fail silently if an error occurrs
+            # this email will be sent to the users email inbox/spam folder
+            user.email_user(subject, message, request.user.get_full_name(),
+                                                    fail_silently=True)
+            
+
+            #file = request.FILES['file']
+            #email.attach(file.name, file.read(), file.content_type())
+
+            #email.send()
+
+            # All emails sent will be viewed on the configured Google Email Account in the settings.py file
+            # Log into the Gmail account of the configured Google Email Account in the settings.py file
+            # Navigate to the sent folders to view all emails sent and any error messages sent back
+
+            # the messages python module simply sends a message back to the HTML template 
+            # rendering this view, for you to display to the user
+            messages.success(request, f'An email has been sent to {user.get_full_name()}')
+
+            # redirect the user back to the home page
+            return redirect('index')
+    # pass the blank email message form when a user navigates to the email page through a GET request
+    else:
+        form = EmailMessageForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'UserHub/send_email.html', context)
+
+
